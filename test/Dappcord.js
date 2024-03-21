9e3f337d1035f16bae608e0628c8c2bd7dc71222
PoliJun <rivenloving@gmail.com>
2024-03-22 03:20:18 +0800

    beforeEach test

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -7,14 +7,23 @@ const tokens = (n) => {
 };
 
 describe("Dappcord", () => {
-    describe("Deployment", async () => {
+    let deployer, user;
+    let dappcord;
+
+    const NAME = "Dappcord";
+    const SYMBOL = "DC";
+    beforeEach(async () => {
+        const Dappcord = await ethers.getContractFactory("Dappcord");
+        dappcord = await Dappcord.deploy(NAME, SYMBOL);
+        await dappcord.deployed();
+    });
+    describe("Deployment", () => {
         it("Sets the name", async () => {
-            const Dappcord = await ethers.getContractFactory("Dappcord");
-            const dappcord = await Dappcord.deploy("Dappcord", "DC");
-            await dappcord.deployed();
             const name = await dappcord.name();
-            const symbol = await dappcord.symbol();
             expect(name).to.equal("Dappcord");
+        });
+        it("Sets the symbol", async () => {
+            const symbol = await dappcord.symbol();
             expect(symbol).to.equal("DC");
         });
     });
